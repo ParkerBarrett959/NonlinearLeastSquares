73145898f7ede3030ff8e815e4cea31eb8ca60e3
Parker Barrett <parkerb7@vt.edu>
2024-12-15 13:43:00 -0500

    Add skeleton for gradient descent optimizer

--- examples/Example1.cpp
+++ examples/Example1.cpp
@@ -2,7 +2,7 @@
  * A Nonlinear Least Squares Solver Example
  *
  * Problem statement: Example 1 contained in this executable is a simple test
- * of the nonlinear least squares solver(s) on a linear objective function. 
+ * of the nonlinear least squares solver(s) on a linear objective function.
  */
 #include "ModelFunctor.h"
 #include "NonlinearOptimizer.h"
---
+++ include/GradientDescent.h
@@ -0,0 +1,40 @@
+#ifndef GRADIENT_DESCENT_H
+#define GRADIENT_DESCENT_H
+
+#include "ModelFunctor.h"
+#include "NonlinearOptimizer.h"
+
+/**
+ * Gradient Descent Nonlinear Optimizer
+ *
+ * @brief: This class inherits from the NonlinearOptimizer interface and
+ * implements a gradient descent approach to solving the least squares problem.
+ */
+class GradientDescent : public NonlinearOptimizer {
+public:
+  /**
+   * c'tor
+   *
+   * @param model A shared pointer to a functor representing the nonlinear model
+   * to be solved.
+   * @param A A vector of parameters to be estimated in the least squares
+   * problem
+   * @param X An nxm matrix of model independent variables. Each row corresponds
+   * to an independent data point, while each column corresponds to the set of
+   * independent variables in a single data point.
+   * @param Y An n dimensional vector of dependent variables
+   */
+  GradientDescent(std::shared_ptr<ModelFunctor> &model,
+                  const Eigen::VectorXd &A, const Eigen::MatrixXd &X,
+                  const Eigen::VectorXd &Y)
+      : NonlinearOptimizer(model, A, X, Y) {}
+
+  /**
+   * Functionm to run the gradient descent optimization
+   *
+   * @return True if the optimization succeeded, false otherwise
+   */
+  bool optimize() { return true; }
+};
+
+#endif // GRADIENT_DESCENT_H
